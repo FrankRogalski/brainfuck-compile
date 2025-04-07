cf345a11c31efa2a2368e6e2fd307d76bf408e89
Frank Rogalski <rogalski.frank@web.de>
2025-04-07 18:29:30 +0200

    moved tmp.c to stdin

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::{
     env::args,
     fs::File,
     io::{BufReader, Read, Write},
-    process::Command,
+    process::{Command, Stdio},
 };
 
 fn main() {
@@ -10,34 +10,50 @@ fn main() {
 
     let mut program: Vec<u8> = vec![];
     program.extend(
-        b"
-        #include <stdint.h>
-        #include <stdio.h>
-        int main() {
-        uint16_t index = 0;
-        char band[0x10000] = {0};
-        ",
+        b"#include <stdint.h>
+#include <stdio.h>
+
+int main() {
+    uint16_t index = 0;
+    char band[0x10000] = {0};
+",
     );
 
     let file = BufReader::new(File::open(path).expect("file needs to exist"));
+    let mut depth = 1;
     for chr in file.bytes() {
         let chr = chr.unwrap();
+        for _ in 0..depth {
+            program.extend(b"    ");
+        }
         program.extend(match chr {
             b'>' => b"index++;\n" as &[u8],
             b'<' => b"index--;\n",
             b'+' => b"band[index]++;\n",
             b'-' => b"band[index]--;\n",
-            b'[' => b"while (band[index]) {\n",
-            b']' => b"}\n",
+            b'[' => {
+                depth += 1;
+                b"while (band[index]) {\n"
+            }
+            b']' => {
+                depth -= 1;
+                b"}\n"
+            }
             b'.' => b"printf(\"%c\", band[index]);\n",
             b',' => b"band[index] = getc();\n",
             _ => b"",
         });
     }
     program.extend(b"return 0;\n}");
-    File::create("tmp.c").unwrap().write_all(&program).unwrap();
-    Command::new("gcc")
-        .args(["-O3", "-o", "bf", "tmp.c"])
-        .output()
+    let mut child = Command::new("gcc")
+        .stdin(Stdio::piped())
+        .args(["-O3", "-x", "c", "-o", "bf", "-"])
+        .spawn()
         .unwrap();
+
+    if let Some(mut stdin) = child.stdin.take() {
+        stdin.write_all(&program).unwrap();
+    }
+
+    child.wait().unwrap();
 }
